8fc36c569ee53092165f7c1d9d5c1b1fc970b147
Enes Sadık Özbek <es.ozbek@outlook.com>
2019-03-30 20:34:43 +0300

    Fix RocketLauncher name

--- start_gameserver.sh
+++ start_gameserver.sh
@@ -3,4 +3,4 @@ cd $GAME_INSTALL_DIR
 ulimit -n 2048
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
-dotnet "RocketUnturned.dll" server Unturned_Headless.x86_64
+dotnet "RocketLauncher.dll" server Unturned_Headless.x86_64
