77875ed467b4fcbbd8d2044d41a6149ff1f670ed
dvdblk <github@dvdblk.com>
2021-02-09 22:18:41 +0000

    Fix init.sh steamclient.so path

--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 SCRIPT_DIR=$PWD
 curl -s https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz | tar -vxz
-cp -f linux64/steamclient.so Unturned_Headless_Data/Plugins/x86_64/steamclient.so
 
 # Update / install server
 ./steamcmd.sh +login $STEAM_USERNAME $STEAM_PASSWORD $STEAM_GUARD_TOKEN $STEAM_CMD_ARGS +force_install_dir $GAME_INSTALL_DIR +@sSteamCmdForcePlatformBitness 64 +app_update $GAME_ID +quit
+
+# Move the steamclient
+mkdir -p /home/steam/.steam/sdk64/
+cp -f linux64/steamclient.so /home/steam/.steam/sdk64/steamclient.so
 
 # Optionlly install RocketMod
 export MODULES_DIR=$GAME_INSTALL_DIR/Modules
